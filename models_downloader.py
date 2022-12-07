0683808bee04397225417cee08198aca69495608
Megadeth2006 <danielgrigorevdlol@gmail.com>
2022-12-07 21:28:15 +0300

    Надо доделывать

---
+++ models_downloader.py
@@ -0,0 +1,5 @@
+import wget
+
+
+wget.download("https://github.com/Shahnawax/HAR-CNN-Keras/raw/master/model.h5")
+wget.download("https://github.com/patrick013/Object-Detection---Yolov3/raw/master/model/yolov3.weights")
